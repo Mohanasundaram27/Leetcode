217399b112f4c7d1475592918f26271d0dc9fba5
Mohanasundaram27 <107344273+Mohanasundaram27@users.noreply.github.com>
2024-03-29 17:35:43 +0530

    Time: 48 ms (42.18%), Space: 12.7 MB (74.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,15 +5,13 @@ public:
         {
             for(int j=i+1;j<nums.size();j++)
             {
-                if(nums[i]+nums[j]==target)
-                {
-                return {i,j};
+                if(nums[i]+nums[j]==target){
+            
+                    return {i,j};
                 }
+    
             }
         }
-            
-        return {};
+          return {};
     }
-
-
 };

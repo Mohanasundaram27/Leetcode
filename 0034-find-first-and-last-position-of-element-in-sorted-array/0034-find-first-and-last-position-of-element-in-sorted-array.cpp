c3b72b7ff9d5813f39f4bb4b84de07b5a2430da4
Mohanasundaram27 <107344273+Mohanasundaram27@users.noreply.github.com>
2024-03-29 18:45:36 +0530

    Time: 7 ms (48.31%), Space: 16.1 MB (12.33%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& v, int target) {
-        // Initialize an empty vector to store indices where the target is found
-        vector<int> mp;
-        // Initialize another vector to store the result
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
         vector<int> k;
-        
-        // Iterate through the input vector
-        for(int i = 0; i < v.size(); i++) {
-            // If the current element equals the target, store its index in 'mp'
-            if(v[i] == target) {
-                mp.push_back(i);
+         for(int i=0;i<nums.size();i++){
+            if(nums[i]==target)
+            {
+                ans.push_back(i);
             }
+            
+         }
+        if(!ans.empty())
+        {
+            k.push_back(ans[0]);
+            k.push_back(ans[ans.size()-1]);
         }
-        
-        // If 'mp' is not empty, store the first and last indices of 'mp' in 'k'
-        if(!mp.empty()) {
-            k.push_back(mp[0]); // First index
-            k.push_back(mp[mp.size()-1]); // Last index
+        else
+        {
+            return {-1,-1};
         }
-        // If 'mp' is empty, store {-1, -1} in 'k'
-        else {
-            k = {-1, -1};
-        }
-        
-        // Return the result vector 'k'
         return k;
+    
+        
+        
+    
+    
+            
+        
     }
 };

1e01da7ac7bab70e5031a740ffaffac53290e2f5
Mohanasundaram27 <107344273+Mohanasundaram27@users.noreply.github.com>
2024-06-25 21:09:46 +0530

    Time: 77 ms (58.96%), Space: 70.2 MB (29.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,14 +5,14 @@ public:
         int sum=0;
         for(int i=0;i<nums.size();i++)
         {
-          sum+=nums[i];
+            sum+=nums[i];
+            
             if(sum>maxi)
-            {
+            
                 maxi=sum;
-            }
+           
             if(sum<0)
                 sum=0;
-                
         }
         return maxi;
         

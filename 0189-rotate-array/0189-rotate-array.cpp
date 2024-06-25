eabe0fede03a9663675c4ba8a4d415b30e7f310d
Mohanasundaram27 <107344273+Mohanasundaram27@users.noreply.github.com>
2024-06-25 12:42:19 +0530

    Time: 22 ms (46.63%), Space: 27.3 MB (55.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (k == n) {
-            return;
+        int n=nums.size();
+        if(k==n)
+            return ;
+        
+        k=k%n;
+        
+        
+        int left=0;
+        int right=n-1-k;
+        int nextleft=right+1;
+        while(left<right)
+        {
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
         }
-        if (k > n) {
-            k = k % n;
+        right=nums.size()-1;
+        while(nextleft<right)
+        {
+            int temp=nums[nextleft];
+            nums[nextleft]=nums[right];
+            nums[right]=temp;
+            nextleft++;
+            right--;
         }
-        vector<int> ans(n);
-        int j = 0;
-        for (int i = n - k; i < n; ++i) {
-            ans[j++] = nums[i];
+        left=0;
+        right=nums.size()-1;
+        while(left<right)
+        {
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+                 
         }
-        for (int i = 0; i < n - k; ++i) {
-            ans[j++] = nums[i];
-        }
-        for (int i = 0; i < n; ++i) {
-            nums[i] = ans[i];
-        }
+        
     }
 };
